d4a55874b3545352550612312264dd7f19e5ec13
Zero Delta <zerodeltahedge@gmail.com>
2020-10-30 13:06:27 +0200

    Revert "leap solution"
    This reverts commit 67117041b167c4228f67907440bd640ab3a02fe1.

--- leap/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub fn is_leap_year(year: u64) -> bool {
-    
-    match (year % 400, year % 100, year % 4) {
-        (0, 0, 0) => true,
-        (_, 0, 0) => false,
-        (_, _, 0) => true,
-        (_, _, _) => false
-    }
-}
