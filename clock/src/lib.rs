00cd5a8daf54759def93228788c85acf1b7351f0
Zero Delta <zerodeltahedge@gmail.com>
2020-11-25 04:37:03 +0200

    Update lib.rs

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,15 +1,33 @@
-pub struct Clock;
+use chrono::{Duration, NaiveTime};
+use std::fmt;
+
+pub struct Clock{
+    time: NaiveTime
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        Clock {
+            time: NaiveTime::from_hms(0, 0, 0)
+            + Duration::hours(hours as i64)
+            + Duration::minutes(minutes as i64)
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+        Clock {
+            time: self.time + Duration::minutes(minutes as i64)
+        }
+    }
+}
+
+// impl fmt::Display for Clock {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         let _time = &self.time.format("%H:%M").to_string();
+//         write!(f, "{}", _time)
+//     }
+impl ToString for Clock {
+    fn to_string(&self) -> String {
+        format!("{}", &self.time.format("%H:%M").to_string())
     }
 }
