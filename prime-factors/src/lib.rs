8c17c9eea9f51b5985b4a588b4f7501b573a640e
Zero Delta <zerodeltahedge@gmail.com>
2020-12-16 02:07:17 +0200

    prime-factors solution

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,3 +1,17 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    unimplemented!("This should calculate the prime factors of {}", n)
+    
+    let mut num = n;
+    let mut pfactors = vec![];
+    let mut d = 2;
+
+    while num > 1 {
+        if num % d != 0 {
+            d += 1;
+        } else {
+            pfactors.push(d);
+            num /= d;
+        }
+    }
+
+    pfactors
 }
