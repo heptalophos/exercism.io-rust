5099e2f6692397e86e08b11b352b1d32e2dc3dae
Zero Delta <zerodeltahedge@gmail.com>
2021-01-29 22:57:20 +0200

    Create lib.rs - sieve

---
+++ sieve/src/lib.rs
@@ -0,0 +1,25 @@
+use std::ops::Range;
+// use Sieve::primes_up_to;
+
+pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
+    if upper_bound < 2 { 
+        vec![] 
+    } else {
+        sieve(2..(upper_bound + 1))
+    }
+}
+
+fn sieve(range: Range<u64>) -> Vec<u64> {
+    eratosthenes(range.collect(), vec![])
+}
+
+fn eratosthenes(mut nrange: Vec<u64>, 
+                mut primes: Vec<u64>) -> Vec<u64> {
+    if nrange.is_empty() { primes }
+    else { 
+        let p = nrange[0];
+        nrange.retain(|&n| n % p != 0 );
+        primes.push(p);
+        eratosthenes(nrange, primes)
+    }
+}
