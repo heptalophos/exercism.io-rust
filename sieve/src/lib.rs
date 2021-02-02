436f99efb4f4abc97bdea1306118f71feb47bf02
Zero Delta <zerodeltahedge@gmail.com>
2021-02-02 17:49:48 +0200

    2nd iteration sieve - update lib.rs

--- sieve/src/lib.rs
+++ sieve/src/lib.rs
@@ -1,25 +1,24 @@
 use std::ops::Range;
-// use Sieve::primes_up_to;
 
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    if upper_bound < 2 { 
-        vec![] 
-    } else {
+    
+    if upper_bound > 1 { 
         sieve(2..(upper_bound + 1))
+    } else {
+        vec![]
     }
 }
 
 fn sieve(range: Range<u64>) -> Vec<u64> {
-    eratosthenes(range.collect(), vec![])
-}
 
-fn eratosthenes(mut nrange: Vec<u64>, 
-                mut primes: Vec<u64>) -> Vec<u64> {
-    if nrange.is_empty() { primes }
-    else { 
-        let p = nrange[0];
-        primes.push(p);
-        nrange.retain(|&n| n % p != 0 );
-        eratosthenes(nrange, primes)
+    let mut eratosthenes = range.collect::<Vec<u64>>();
+    let mut primes = Vec::<u64>::new();
+    
+    while !eratosthenes.is_empty() { 
+        let prime = eratosthenes[0];
+        primes.push(prime);
+        eratosthenes.retain(|n| n % prime != 0);
     }
+    
+    primes
 }
