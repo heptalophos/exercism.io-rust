391bddd754ec79a0117d9f002ae4c2d5107e9308
Zero Delta <zerodeltahedge@gmail.com>
2021-02-08 23:55:50 +0200

    Update lib.rs

--- nucleotide-count/src/lib.rs
+++ nucleotide-count/src/lib.rs
@@ -1,17 +1,11 @@
 use std::collections::HashMap;
 
 pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
-    if !"ACGT".contains(nucleotide) {
-        Err(nucleotide)
-    } else {
-        dna.chars().try_fold(0, |acc, n| {
-            match ("ACGT".contains(n), n == nucleotide) {
-                (true, true) => Ok(acc + 1),
-                (true, false) => Ok(acc),
-                (_, _) => Err(n),
-            }
-        })
-    }
+    unimplemented!(
+        "How much of nucleotide type '{}' is contained inside DNA string '{}'?",
+        nucleotide,
+        dna
+    );
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
