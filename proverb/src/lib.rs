bc74df2902d4b5499c94af326ff119590e854339
Zero Delta <zerodeltahedge@gmail.com>
2021-02-09 17:49:41 +0200

    proverb solution (1st iteration)   lib.rs

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,3 +1,27 @@
+use std::iter::once;
+
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+ 
+    if list.is_empty() {
+        return String::new()
+    }
+    
+    let couplets = 
+        list.iter()
+            .copied()
+            .zip(list.iter()
+                     .copied()
+                     .skip(1));
+    
+    let prologue = 
+        couplets.map(|(premise, conclusion)|
+                     format!("For want of a {} the {} was lost.",
+                             premise, conclusion));
+    
+    let epilogue =
+        format!("And all for the want of a {}.", list[0]);
+
+    prologue.chain(once(epilogue))
+            .collect::<Vec<_>>()
+            .join("\n")
 }
