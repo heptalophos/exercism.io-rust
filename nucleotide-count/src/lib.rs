6c95d8eecf20c30c31ac78a1f9efd1187e5fed3b
Zero Delta <zerodeltahedge@gmail.com>
2021-02-09 16:41:07 +0200

    Refactor nucleotide-count lib.rs

--- nucleotide-count/src/lib.rs
+++ nucleotide-count/src/lib.rs
@@ -1,16 +1,24 @@
 use std::collections::HashMap;
 
 pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
-    unimplemented!(
-        "How much of nucleotide type '{}' is contained inside DNA string '{}'?",
-        nucleotide,
-        dna
-    );
+    let valid = |x| if "ACGT".contains(x) {1} else {0};
+    let exact = |x, y| if x == y {1} else {0};
+
+    if valid(nucleotide) != 0 {
+        dna.chars().try_fold(0, |acc, n| {
+            match (valid(n), exact(nucleotide, n)) {
+                    (1, 1) => Ok(acc + 1),
+                    (1, 0) => Ok(acc),
+                    (_, _) => Err(n),
+            }
+        })
+    } else {
+        Err(nucleotide)
+    }
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
-    unimplemented!(
-        "How much of every nucleotide type is contained inside DNA string '{}'?",
-        dna
-    );
+    "ACGT".chars()
+          .map(|n| Ok((n, count(n, dna)?)))
+          .collect()
 }
