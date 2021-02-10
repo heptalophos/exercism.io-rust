0c87c4aa10a2348adc3f409ae0b24b65082e0ffa
Zero Delta <zerodeltahedge@gmail.com>
2021-02-10 23:44:17 +0200

    Tentative 1st iteration rna-transcription

--- rna-transcription/src/lib.rs
+++ rna-transcription/src/lib.rs
@@ -1,21 +1,53 @@
 #[derive(Debug, PartialEq)]
-pub struct Dna;
+pub struct Dna {
+    deoxyribonucleic_strand: String,
+}
 
 #[derive(Debug, PartialEq)]
-pub struct Rna;
+pub struct Rna {
+    ribonucleic_strand: String,
+}
 
 impl Dna {
     pub fn new(dna: &str) -> Result<Dna, usize> {
-        unimplemented!("Construct new Dna from '{}' string. If string contains invalid nucleotides return index of first invalid nucleotide", dna);
+        for (index, dorn) in dna.chars().enumerate() {
+            match dorn {
+                'A' | 'C' | 'G' | 'T' => {}
+                _ => return Err(index),
+            }
+        }
+        Ok(Dna {
+            deoxyribonucleic_strand: dna.chars().as_str().to_string(),
+        })    
     }
 
     pub fn into_rna(self) -> Rna {
-        unimplemented!("Transform Dna {:?} into corresponding Rna", self);
+        Rna {
+            ribonucleic_strand: self
+                                .deoxyribonucleic_strand
+                                .chars()
+                                .map (|n| match n {
+                                    'G' => 'C',
+                                    'C' => 'G',
+                                    'T' => 'A',
+                                    'A' => 'U',
+                                    _ => '$',
+                                })
+                                .collect(),
+        }
     }
 }
 
 impl Rna {
     pub fn new(rna: &str) -> Result<Rna, usize> {
-        unimplemented!("Construct new Rna from '{}' string. If string contains invalid nucleotides return index of first invalid nucleotide", rna);
+        for (index, rn) in rna.chars().enumerate() {
+            match rn {
+                'A' | 'C' | 'G' | 'U' => {}
+                _ => return Err(index),
+            }
+        }
+        Ok(Rna {
+            ribonucleic_strand: rna.chars().as_str().to_string(),
+        })     
     }
 }
