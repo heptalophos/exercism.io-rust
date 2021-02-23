6df4ceac9e40ee0907b3b0bbb91109276bb722c3
Zero Delta <zerodeltahedge@gmail.com>
2021-02-23 17:15:07 +0200

    matching-brackets lib.rs

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -1,6 +1,17 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!(
-        "Check if the string \"{}\" contains balanced brackets",
-        string
-    );
+    
+    let mut stack: Vec<char> = vec![];
+
+    for ch in string.chars() {
+        match ch {
+            '}' | ']' | ')' 
+                if Some(ch) != stack.pop() =>
+                        return false,
+            '{' => stack.push('}'),
+            '[' => stack.push(']'),
+            '(' => stack.push(')'),
+            _   => (),
+        }
+    }
+    return stack.is_empty()
 }
