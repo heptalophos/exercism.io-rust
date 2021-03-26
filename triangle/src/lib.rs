bac1055620558bb73e145e0a4c3dd1a308a61b66
Zero Delta <zerodeltahedge@gmail.com>
2021-03-26 23:53:23 +0200

    Refactor triangle

--- triangle/src/lib.rs
+++ triangle/src/lib.rs
@@ -6,24 +6,9 @@ pub struct Triangle {
 
 impl Triangle {
 
-    // pub fn any_side_zero(&self) -> bool {
-    //     if self.sides.iter().any(|&s| s == 0) {
-    //         return true
-    //     }
-    //     false
-    // }
-
-    // pub fn triangle_inequality(&self) -> bool {
-    //     // let sides = self.sides;
-    //     if self.sides.iter().max().unwrap() < 
-    //        self.sides.iter().fold(&0u64, |s, &v| {s + v; s})
-    //        { return true }
-    //     false
-    // }
-
     pub fn build(sides: [u64; 3]) -> Option<Triangle> {
         let triangle_inequality: bool =
-            2u64 * sides.iter().max().unwrap() < 
+            2 * sides.iter().max().unwrap() < 
             sides.iter().sum::<u64>();
         let any_side_zero: bool=
             sides.iter().any(|s| *s == 0);
