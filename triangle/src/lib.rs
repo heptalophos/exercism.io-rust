02413c17c67cff46c0ccb560321a6379173ad020
Zero Delta <zerodeltahedge@gmail.com>
2021-03-27 17:22:23 +0200

    Refactor triangle lib.rs

--- triangle/src/lib.rs
+++ triangle/src/lib.rs
@@ -7,34 +7,35 @@ pub struct Triangle {
 impl Triangle {
 
     pub fn build(sides: [u64; 3]) -> Option<Triangle> {
+
         let triangle_inequality: bool =
-            2 * sides.iter().max().unwrap() < 
+            2 * sides.iter().max().unwrap() 
+	        <= 
             sides.iter().sum::<u64>();
         let any_side_zero: bool=
-            sides.iter().any(|s| *s == 0);
+            sides.iter().any(|side| *side == 0);
 
         if !triangle_inequality || any_side_zero {
             return None;
         }
-        Some(Triangle {sides: sides} )
+        Some( Triangle {sides: sides} )
     }
 
-    pub fn distinct_sides(&self) -> u32 {
-        let sides = 
-            self.sides.iter().copied()
-                .collect::<HashSet<u64>>();
-        sides.len() as u32
+    pub fn distinct_sides(&self) -> u8 {
+          self.sides.iter().copied()
+              .collect::<HashSet<u64>>()
+              .len() as u8
     }
 
     pub fn is_equilateral(&self) -> bool {
-        self.distinct_sides() == 1u32 
+        self.distinct_sides() == 1 
     }
 
     pub fn is_isosceles(&self) -> bool {
-        self.distinct_sides() <= 2u32
+        self.distinct_sides() <= 2
     }
 
     pub fn is_scalene(&self) -> bool {
-        self.distinct_sides() == 3u32
+        self.distinct_sides() == 3
     }
 }
