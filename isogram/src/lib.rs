732141b62902b52c0ea0009251e410d205042ea7
Zero Delta <zerodeltahedge@gmail.com>
2021-04-10 02:39:00 +0300

    Isogram lib.rs - 1st iteration

--- isogram/src/lib.rs
+++ isogram/src/lib.rs
@@ -9,5 +9,5 @@ pub fn check(candidate: &str) -> bool {
              .filter(|x| x.is_alphabetic())
              .for_each(|x| *counts.entry(x).or_insert(0) += 1);
     
-    counts.iter().find(|(_, c)| *c > &1).is_none()
+    counts.iter().find(|(_, c)| *c > &1u16).is_none()
 }          
