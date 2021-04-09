fe36a1cc9178bce0b3f2240a88948479e80c1070
Zero Delta <zerodeltahedge@gmail.com>
2021-04-10 02:28:03 +0300

    Update lib.rs

--- isogram/src/lib.rs
+++ isogram/src/lib.rs
@@ -4,4 +4,10 @@ pub fn check(candidate: &str) -> bool {
     
     let mut counts: HashMap<char, u16> = HashMap::new();
 
-}         
+    candidate.to_lowercase()
+             .chars()
+             .filter(|x| x.is_alphabetic())
+             .for_each(|x| *counts.entry(x).or_insert(0) += 1);
+    
+    candidate.is_empty() || !counts.iter().any(|(_, c)| *c > 1)
+}          
