375e725d6869f00ec95b4cc637d3e8c32b132921
Zero Delta <zerodeltahedge@gmail.com>
2021-04-11 16:29:03 +0300

    Refactor isogram

--- isogram/src/lib.rs
+++ isogram/src/lib.rs
@@ -5,9 +5,9 @@ pub fn check(candidate: &str) -> bool {
     let mut counts: HashMap<char, u16> = HashMap::new();
 
     candidate.to_lowercase()
-             .chars()
-             .filter(|x| x.is_alphabetic())
-             .for_each(|x| *counts.entry(x).or_insert(0) += 1);
+    .chars().filter(|x| x.is_alphabetic())
+    .for_each(|x| *counts.entry(x).or_insert(0) += 1);
     
-    counts.iter().find(|(_, c)| *c > &1u16).is_none()
+    candidate.is_empty() || 
+    counts.iter().find(|(_, c)| *c > &1).is_none()
 }          
