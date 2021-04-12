e24c9d9533e751bd21a08462d7320ebc49ff9bfd
Zero Delta <zerodeltahedge@gmail.com>
2021-04-12 17:12:46 +0300

    Update perfect-numbers lib.rs

--- perfect-numbers/src/lib.rs
+++ perfect-numbers/src/lib.rs
@@ -6,5 +6,22 @@ pub enum Classification {
 }
 
 pub fn classify(num: u64) -> Option<Classification> {
-    unimplemented!("classify {}", num);
+    
+    let aliquot: u64= 
+        (1..=(num / 2))
+        .filter(|i| num % i == 0)
+        .fold(0, |sum, i| sum + i);
+
+        if num != 0 {
+            if aliquot > num {
+                return Some(Classification::Abundant)
+            }
+            if aliquot == num {
+                return  Some(Classification::Perfect)
+            }
+            if aliquot < num {
+                return Some(Classification::Deficient)
+            }
+        }   
+        return None
 }
