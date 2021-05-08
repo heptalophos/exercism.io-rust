b5de5dad67690bb59deaebc1577ea8c1c91652c0
Zero Delta <zerodeltahedge@gmail.com>
2021-05-08 18:42:13 +0300

    Reformat rna-transcription

--- rna-transcription/src/lib.rs
+++ rna-transcription/src/lib.rs
@@ -10,44 +10,51 @@ pub struct Rna {
 
 impl Dna {
     pub fn new(dna: &str) -> Result<Dna, usize> {
-        for (index, dorn) in dna.chars().enumerate() {
+        for (idx, dorn) in dna.chars().enumerate() {
             match dorn {
                 'A' | 'C' | 'G' | 'T' => {}
-                _ => return Err(index),
+                _ => return Err(idx),
             }
         }
         Ok(Dna {
-            deoxyribonucleic_strand: dna.chars().as_str().to_string(),
+            deoxyribonucleic_strand: 
+                    dna.chars()
+                       .as_str()
+                       .to_string(),
         })    
     }
 
     pub fn into_rna(self) -> Rna {
         Rna {
-            ribonucleic_strand: self
-                                .deoxyribonucleic_strand
-                                .chars()
-                                .map (|n| match n {
-                                    'G' => 'C',
-                                    'C' => 'G',
-                                    'T' => 'A',
-                                    'A' => 'U',
-                                    _ => '$',
-                                })
-                                .collect(),
+            ribonucleic_strand: 
+                    self
+                    .deoxyribonucleic_strand
+                    .chars()
+                    .map (|n| match n {
+                            'G' => 'C',
+                            'C' => 'G',
+                            'T' => 'A',
+                            'A' => 'U',
+                             _  => '$',
+                    })
+                    .collect(),
         }
     }
 }
 
 impl Rna {
     pub fn new(rna: &str) -> Result<Rna, usize> {
-        for (index, rn) in rna.chars().enumerate() {
+        for (idx, rn) in rna.chars().enumerate() {
             match rn {
                 'A' | 'C' | 'G' | 'U' => {}
-                _ => return Err(index),
+                _ => return Err(idx),
             }
         }
         Ok(Rna {
-            ribonucleic_strand: rna.chars().as_str().to_string(),
+            ribonucleic_strand: 
+                    rna.chars()
+                       .as_str()
+                       .to_string(),
         })     
     }
 }
