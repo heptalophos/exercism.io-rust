5a654db7c90f17aae16b7a0956fcec8753eedbea
Zero Delta <zerodeltahedge@gmail.com>
2021-05-19 23:51:59 +0300

    Reformat lib.rs - sum-of-multiples

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,9 +1,9 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
     
     (1..limit)
-    .filter(|i| -> bool { factors.iter()
-                                 .filter(|f| **f > 0)
-                                 .any(|f| i % f == 0) })
-    .fold(0, 
-          |acc, i|  acc + i )
+    .filter(|i| -> bool { 
+                    factors.iter()
+                           .filter(|f| **f > 0)
+                           .any(|f| i % f == 0) })
+    .fold(0, |acc, i|  acc + i )
 }
