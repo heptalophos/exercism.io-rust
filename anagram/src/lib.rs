8015aca7ba5dbb30da2c49e0e16e92ad325ad40c
Zero Delta <zerodeltahedge@gmail.com>
2021-05-31 17:27:05 +0300

    Refactor anagram\src\lib.rs

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -2,27 +2,24 @@ use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, 
                         possible_anagrams: &[& 'a str]) -> 
-                                        HashSet<&'a str> {
-    possible_anagrams.iter()
-                     .filter(|&w| is_anagram(w, word))
-                     .copied()
-                     .collect()
-}
-
-fn is_anagram(candidate: &str, word: &str) -> bool {
-    if candidate.len() == word.len() {
-        let mut cand_v =
-                candidate.to_lowercase()
-                         .chars()
-                         .collect::<Vec<char>>();
-        let mut word_v =
-                word.to_lowercase()
-                    .chars()
-                    .collect::<Vec<char>>();
-        cand_v.sort();
-        word_v.sort();
-        return candidate.to_lowercase() != word.to_lowercase() 
-               && cand_v == word_v;
-    }
-    return false
+                                            HashSet<&'a str> {
+    let mut word_v = word.to_lowercase()
+                           .chars()
+                           .collect::<Vec<char>>();
+    word_v.sort_unstable();
+    
+    possible_anagrams
+    .iter()
+    .filter(|&&candidate| {
+        candidate.to_lowercase() != word.to_lowercase() && {
+            let mut cand_v = candidate
+                             .to_lowercase()
+                             .chars()
+                             .collect::<Vec<char>>();
+            cand_v.sort_unstable();
+            word_v == cand_v
+        }
+    })
+    .copied()
+    .collect()
 }
