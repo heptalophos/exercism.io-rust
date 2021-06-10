8353ed0a3ec66411027da199da5e983ed8f2c267
Zero Delta <zerodeltahedge@gmail.com>
2021-06-10 21:05:27 +0300

    Reformat lib.rs

--- protein-translation/src/lib.rs
+++ protein-translation/src/lib.rs
@@ -2,12 +2,14 @@ use std::collections::HashMap;
 
 const STOP: &str = "stop codon";
 
-pub struct CodonsInfo<'a>(HashMap<&'a str, &'a str>);
+pub struct CodonsInfo<'a> (HashMap<&'a str, &'a str>);
 
 impl<'a> CodonsInfo<'a> {
     pub fn name_for(&self, codon: &str) -> 
     Option<&'a str> {
-        self.0.get(codon).copied()
+        self.0
+            .get(codon)
+            .copied()
     }
 
     pub fn of_rna(&self, rna: &str) -> 
@@ -26,6 +28,8 @@ impl<'a> CodonsInfo<'a> {
 pub fn parse<'a>(pairs: Vec<(&'a str, &'a str)>) ->
 CodonsInfo<'a> {
     CodonsInfo (
-        pairs.iter().copied().collect(),
+        pairs.iter()
+             .copied()
+             .collect(),
     )
 }
