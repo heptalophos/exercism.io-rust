64ece47945303cb3e5f80d0cfa343ac302d7e4c4
Zero Delta <zerodeltahedge@gmail.com>
2021-06-23 23:41:11 +0300

    scrabble-score\src\lib.rs, 1st iteration

--- scrabble-score/src/lib.rs
+++ scrabble-score/src/lib.rs
@@ -1,4 +1,17 @@
-/// Compute the Scrabble score for a word.
-pub fn score(word: &str) -> u64 {
-    unimplemented!("Score {} in Scrabble.", word);
+pub fn score(word: &str) -> usize {
+    
+    let mut score = 0;
+    for letter in word.to_ascii_uppercase().chars() {
+        score += match letter {
+            x if "AEIOULNRST".contains(x) => 1,
+            x if "DG".contains(x) => 2,
+            x if "BCMP".contains(x) => 3,
+            x if "FHVWY".contains(x) => 4,
+            x if "K".contains(x) => 5,
+            x if "JX".contains(x) => 8,
+            x if "QZ".contains(x) => 10,
+            _ => 0,
+        }
+    }
+    score
 }
