246ab72e49acc4a714e5e23a194a8da073fd20b0
Zero Delta <zerodeltahedge@gmail.com>
2021-07-16 17:43:26 +0300

    Reformat sublist\src\lib.rs

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -11,15 +11,18 @@ pub enum Comparison {
 pub fn sublist<T: PartialEq>(first_list: &[T], second_list: &[T]) -> Comparison {
     use Comparison::*;
     match first_list.len().cmp(&second_list.len()) {
-        Ordering::Equal if first_list == second_list => Equal,
-        Ordering::Less if contained(first_list, second_list) => Sublist,
-        Ordering::Greater if contained(second_list, first_list) => Superlist,
+        Ordering::Equal 
+            if first_list == second_list => Equal,
+        Ordering::Less 
+            if contained(first_list, second_list) => Sublist,
+        Ordering::Greater 
+            if contained(second_list, first_list) => Superlist,
         _ => Unequal,
     }
 }
 
 fn contained<T: PartialEq>(alist: &[T], blist: &[T]) -> bool {
-    alist.is_empty() 
-        || blist.windows(alist.len())
-                 .any(|window| window == alist)
+    alist.is_empty() ||
+    blist.windows(alist.len())
+         .any(|window| window == alist)
 }
