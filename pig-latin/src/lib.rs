e23393f64307373ca4aedc64b1726b6b2f61e15f
Zero Delta <zerodeltahedge@gmail.com>
2021-07-20 01:19:18 +0300

    Working pig-latin\src\lib.rs - 1st iteration

--- pig-latin/src/lib.rs
+++ pig-latin/src/lib.rs
@@ -1,13 +1,19 @@
 pub fn translate(input: &str) -> String {
-    unimplemented!(
-        "Using the Pig Latin text transformation rules, convert the given input '{}'",
-        input
-    );
+    input.split_ascii_whitespace()
+         .map(piglatinize)
+         .collect::<Vec<_>>()
+         .join(" ")
 }
 
-fn piglatinize(word: &str) -> String -> {
-    let mut i = word.find(|c : char| "aeiou".contains(c)).unwrap();
-    if i > 0 && &word[i - 1..i + 1] == "qu" { i += 1}
-    if &word[..2] == "yt" || &word[i..] == "ay" { i == 0 }
+fn piglatinize(word: &str) -> String {
+    let mut i : usize = 
+        word.find(|c : char| "aeiouy".contains(c))
+            .unwrap();
+    let rule1 : bool = i == 0 && & word[..1] == "y";
+    let rule2 : bool = i > 0 && &word[i - 1..i + 1] == "qu";
+    let rule3 : bool = &word[..2] == "yt";
+    let rule4 : bool = &word[i..] == "ay";
+    if  rule1 || rule2 { i += 1 }
+    if  rule3 || rule4 { i = 0 }
     format!("{}{}ay", &word[i..], &word[..i])
 }
