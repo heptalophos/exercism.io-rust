e1834633727ad56e1b77a3749ca3fbfdecefc623
Zero Delta <zerodeltahedge@gmail.com>
2021-07-19 23:59:12 +0300

    Update lib.rs

--- pig-latin/src/lib.rs
+++ pig-latin/src/lib.rs
@@ -6,5 +6,8 @@ pub fn translate(input: &str) -> String {
 }
 
 fn piglatinize(word: &str) -> String -> {
-    let mut 
+    let mut i = word.find(|c : char| "aeiou".contains(c)).unwrap();
+    if i > 0 && &word[i - 1..i + 1] == "qu" { i += 1}
+    if &word[..2] == "yt" || &word[i..] == "ay" { i == 0 }
+    format!("{}{}ay", &word[i..], &word[..i])
 }
