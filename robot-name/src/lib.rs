9a853a7f1aadc4ef023858aa3ab4f069d4d464d6
Zero Delta <zerodeltahedge@gmail.com>
2021-08-01 23:47:51 +0300

    robot-name/src/lib.rs - 1st working iteration

--- robot-name/src/lib.rs
+++ robot-name/src/lib.rs
@@ -1,15 +1,33 @@
-pub struct Robot;
+use rand::Rng;
+
+pub struct Robot {
+    name: String,
+}
 
 impl Robot {
     pub fn new() -> Self {
-        unimplemented!("Construct a new Robot struct.");
+        Self {
+            name: Robot::generate_name(),
+        }
     }
 
     pub fn name(&self) -> &str {
-        unimplemented!("Return the reference to the robot's name.");
+        &self.name
     }
 
     pub fn reset_name(&mut self) {
-        unimplemented!("Assign a new unique name to the robot.");
+        self.name = Robot::generate_name()
+    }
+
+    fn generate_name() -> String {
+        let mut rng = rand::thread_rng();
+        let mut new_name = String::new();
+        for i in 0..=1 {
+            new_name.insert(i, rng.gen_range('A'..='Z'));
+        }
+        for i in 2..=4 {
+            new_name.insert(i, rng.gen_range('0'..='9'));
+        }
+        new_name
     }
 }
