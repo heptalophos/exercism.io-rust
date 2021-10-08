c568a4ed14d356868d44ff209340c0c0c5e92c59
Zero Delta <zerodeltahedge@gmail.com>
2021-10-08 18:17:44 +0300

    Update lib.rs

--- assembly-line/src/lib.rs
+++ assembly-line/src/lib.rs
@@ -3,9 +3,11 @@
 #![allow(unused)]
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
+
     unimplemented!("calculate hourly production rate at speed: {}", speed)
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
+    
     unimplemented!("calculate the amount of working items at speed: {}", speed)
 }
