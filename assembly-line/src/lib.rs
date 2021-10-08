fd453f4a1c9eb4c7f8df3ed13b4a6e8651dd884b
Zero Delta <zerodeltahedge@gmail.com>
2021-10-08 18:41:34 +0300

    Update lib.rs

--- assembly-line/src/lib.rs
+++ assembly-line/src/lib.rs
@@ -1,13 +1,19 @@
-// This stub file contains items which aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
+const MAX_PRODUCTION_PER_HOUR: f64 = 221f64;
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    let success_rate: f64 = match speed {
+        1..=4  => 1.0,
+        5..=8  => 0.9,
+        9 | 10 => 0.77,
+        _      => 0.0
+    };
+    
+    let true_speed: f64 = 
+        speed as f64 * success_rate;
+    
+        MAX_PRODUCTION_PER_HOUR * true_speed
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    production_rate_per_hour(speed) as u32 / 60u32
 }
