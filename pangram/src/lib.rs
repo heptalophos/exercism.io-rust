afce7d71e7f56c9aa1318182222e8efa5afbd0de
Zero Delta <zerodeltahedge@gmail.com>
2021-10-18 20:07:04 +0300

    Update pangram\src\lib.rs

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -3,11 +3,9 @@ use std::collections::HashSet as Set;
 pub fn is_pangram(sentence: &str) -> bool {
 
     (b'a'..=b'z').map(char::from).collect::<Set<_>>() ==
-    sentence.to_lowercase()
-            .chars()
-            .fold(Set::new(), 
-                  |mut acc, ch| { 
-                        if ('a'..='z').contains(&ch) 
-                            { acc.insert(ch); }
-                  acc})
+    sentence.to_lowercase().chars()
+    .fold(Set::new(), |mut acc, ch| { 
+                            if ('a'..='z').contains(&ch) { 
+                                    acc.insert(ch); 
+                            } acc })
 }
