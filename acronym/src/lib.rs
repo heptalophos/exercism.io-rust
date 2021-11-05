c17c73c1785e01972b7cdb257e8ac8d65be5256d
Zero Delta <zerodeltahedge@gmail.com>
2021-11-05 22:09:13 +0200

    Update lib.rs - acronym

--- acronym/src/lib.rs
+++ acronym/src/lib.rs
@@ -1,17 +1,10 @@
 pub fn abbreviate(phrase: &str) -> String {
-    phrase.split(|c: char| 
-                 c.is_whitespace() || 
-                 c == '_' || 
-                 c == '-' )
-          .flat_map(|w| { w.chars()
-                           .filter(|c| 
-                                   c.is_alphabetic())
+    phrase.split(|c: char| c.is_whitespace() || c == '_' || c == '-' )
+          .flat_map(|w| { w.chars().filter(|c| c.is_alphabetic())
                            .take(1)
                            .chain(w.chars()
-                                   .skip_while(|c| 
-                                        c.is_uppercase())
-                                   .filter(|c| 
-                                       c.is_uppercase()))
+                                   .skip_while(|c| c.is_uppercase())
+                                   .filter(|c| c.is_uppercase()))
                         }        
                    )
           .collect::<String>()
