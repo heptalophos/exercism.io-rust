3de6ea9d89536a2bb0465209bafab7a9013d1a66
Zero Delta <zerodeltahedge@gmail.com>
2021-11-11 23:57:51 +0200

    Reformat gigasecond\src\lib.rs

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -3,8 +3,6 @@ use chrono::Duration;
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    
     let gigasecond: i64 = 10i64.pow(9);
-    
     start + Duration::seconds(gigasecond)
 }
