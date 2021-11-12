cf3a373ae502c0cbf358f13cb046c90c380c4299
Zero Delta <zerodeltahedge@gmail.com>
2021-11-12 22:40:02 +0200

    Refactor gigasecond

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,8 +1,7 @@
-use chrono::{DateTime, Utc};
-use chrono::Duration;
+use time::{Duration, PrimitiveDateTime as DateTime};
 
 // Returns a Utc DateTime one billion seconds after start.
-pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
+pub fn after(start: DateTime) -> DateTime {
     let gigasecond: i64 = 10i64.pow(9);
     start + Duration::seconds(gigasecond)
 }
