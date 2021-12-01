3127687eaf20d245c3364dec97fb3c8410f6eb5d
Zero Delta <zerodeltahedge@gmail.com>
2021-12-01 02:45:13 +0200

    1st passing iteration,  semi_structured_logs

--- semi-structured-logs/src/lib.rs
+++ semi-structured-logs/src/lib.rs
@@ -1,24 +1,22 @@
-// This stub file contains items which aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
 #![allow(unused)]
 
-/// various log levels
 #[derive(Clone, PartialEq, Debug)]
 pub enum LogLevel {
     Info,
     Warning,
     Error,
 }
-/// primary function for emitting logs
+
 pub fn log(level: LogLevel, message: &str) -> String {
-    unimplemented!()
+    let log_level = format!("{:?}", level).to_uppercase();
+    format!("[{}]: {}", log_level, message)
 }
 pub fn info(message: &str) -> String {
-    unimplemented!()
+    log(LogLevel::Info, message)
 }
 pub fn warn(message: &str) -> String {
-    unimplemented!()
+    log(LogLevel::Warning, message)
 }
 pub fn error(message: &str) -> String {
-    unimplemented!()
+    log(LogLevel::Error, message)
 }
