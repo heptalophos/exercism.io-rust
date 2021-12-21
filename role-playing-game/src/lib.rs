0bc493cbaf5d89ce94840f579f165ed0894ce7d5
Zero Delta <zerodeltahedge@gmail.com>
2021-12-21 23:45:19 +0200

    1st passing iteration - role-playing-game\src\lib.rs

--- role-playing-game/src/lib.rs
+++ role-playing-game/src/lib.rs
@@ -1,5 +1,3 @@
-// This stub file contains items which aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
 #![allow(unused)]
 
 pub struct Player {
@@ -9,11 +7,32 @@ pub struct Player {
 }
 
 impl Player {
+    fn new(level: u32) -> Player {
+        Self {
+            health: 100,
+            mana: if level < 10 { None } else { Some(100) },
+            level: level
+        }
+    }
+
     pub fn revive(&self) -> Option<Player> {
-        unimplemented!("Revive this player")
+        if self.health > 0 { return None }
+        Some(Player::new(self.level))
     }
 
     pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
-        unimplemented!("Cast a spell of cost {}", mana_cost)
+        if self.mana.is_none() {
+            if mana_cost <= self.health {
+                self.health -= mana_cost;
+            } else {
+                self.health = 0;
+            }
+            return 0;
+        }
+        if Some(mana_cost) > self.mana {
+            return 0;
+        }
+        self.mana = Some(self.mana.unwrap() - mana_cost);
+        return 2 * mana_cost
     }
 }
