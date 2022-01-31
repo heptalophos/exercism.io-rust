08ad807ddc7c7ba5eca495a466b8b8560b53b839
Zero Delta <zerodeltahedge@gmail.com>
2022-01-31 23:07:05 +0200

    rpn-calculator\src\lib.rs - 1st passing iteration

--- rpn-calculator/src/lib.rs
+++ rpn-calculator/src/lib.rs
@@ -7,31 +7,14 @@ pub enum CalculatorInput {
     Value(i32),
 }
 
-fn operation(stack: &mut Vec<i32>, op: impl Fn(i32, i32) -> i32) -> Option<i32> {
-    stack.pop().and_then(|b| stack.pop().map(|a| op(a, b)))
-}
+// fn operation(stack: &mut Vec<i32>, op: impl Fn(i32, i32) -> i32) -> Option<i32> {
+//     stack.pop().and_then(|b| stack.pop().map(|a| op(a, b)))
+// }
 
 use std::ops::{Add, Div, Mul, Sub};
 
 pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
     let mut stack: Vec<i32> = vec![];
-    // for input in inputs {
-    //     let a = stack.pop()?;
-    //     let b = stack.pop()?;
-    //     let op = stack.pop()?;
-    //     return match op {
-    //         CalculatorInput::Add => Some(i32::add(a, b)),
-    //         CalculatorInput::Subtract => Some(i32::sub(b, a)),
-    //         CalculatorInput::Multiply => Some(i32::mul(a, b)),
-    //         CalculatorInput::Divide => Some(i32::div(b, a)),
-    //         CalculatorInput::Value(n) => Some(n),
-    //     }.map(|x| {
-    //         stack.push(x);
-    //         stack
-    //     }).and_then(|stack| match stack.as_slice() {
-    //         [x] => Some(*x),
-    //         _ => None
-    //         })
     for input in inputs {
         match input {
             CalculatorInput::Add => {
