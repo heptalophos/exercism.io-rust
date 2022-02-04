8587bbff6fd69b3bccba6f00974e9964a55c004d
Zero Delta <zerodeltahedge@gmail.com>
2022-02-04 15:11:10 +0200

    1st passing iter - macros\src\lib.rs

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,6 +1,16 @@
 #[macro_export]
 macro_rules! hashmap {
     () => {
-        unimplemented!()
+        ::std::collections::HashMap::new()
+    };
+    ($($key:expr => $value:expr,)*) => {
+        {   
+            let mut hash_map = macros::hashmap!();
+            $(hash_map.insert($key, $value);)*
+            hash_map
+        }
+    };
+    ($($key:expr => $value:expr),*) => {
+        macros::hashmap!($($key => $value,)*)
     };
 }
