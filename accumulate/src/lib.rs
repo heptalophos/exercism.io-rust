e03d79a243a5e17d6ded5ed6a39e1b856e280017
Zero Delta <zerodeltahedge@gmail.com>
2022-03-25 23:11:08 +0200

    Update lib.rs

--- accumulate/src/lib.rs
+++ accumulate/src/lib.rs
@@ -3,9 +3,3 @@ pub fn map<F: Fn(i32) -> i32>(input: Vec<i32>, function: F) -> Vec<i32> {
     input.iter().map(|x| function(*x)).collect()
 }
 
-
-pub fn map<F: FnMut(A) -> B, A, B>(input: Vec<A>, function: F) -> Vec<B> {
-    input.into_iter()
-         .map(function)
-         .collect()
-}
