c7c22abf99f249f8f6b3aac4cda4cce683daa3ad
Zero Delta <zerodeltahedge@gmail.com>
2022-03-30 20:06:49 +0300

    roman-numerals\src\lib.rs - 1st passing iteration

--- roman-numerals/src/lib.rs
+++ roman-numerals/src/lib.rs
@@ -5,20 +5,19 @@ pub struct Roman (u32);
 impl Display for Roman {
     fn fmt(&self, _f: &mut Formatter<'_>) -> Result {
         let numeral_map: &[(u32, &str)] = 
-            &[ (1, "I"), (4, "IV"), (5, "V"), (9, "IX"),
+            &[ 
+               (1, "I"), (4, "IV"), (5, "V"), (9, "IX"),
                (10, "X"), (40, "XL"), (50, "L"), (90, "XC"),
-               (100, "C"), (400, "CD"), (50, "D"), (900, "CM"), 
+               (100, "C"), (400, "CD"), (500, "D"), (900, "CM"), 
                (1000, "M")
              ];
         let mut arabic = self.0;
         let mut roman = String::new();
         while arabic > 0 {
-            let &(n, r) = 
-                numeral_map.iter().rev()
-                           .filter(|&&(x, _)| x < arabic)
-                           .next()
-                           .unwrap();
-            arabic -= n;
+            let (a, r) = numeral_map.iter().rev()
+                         .filter(|&(n, _)| arabic >= *n)
+                         .next().unwrap();
+            arabic -= a;
             roman.push_str(r);  
         }
         write!(_f, "{}", roman)
