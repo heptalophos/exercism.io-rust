41a256a39406ae1e7543bc7e68cf7c8dadfc48b8
Zero Delta <zerodeltahedge@gmail.com>
2022-05-04 20:16:46 +0300

    Update lib.rs

--- atbash-cipher/src/lib.rs
+++ atbash-cipher/src/lib.rs
@@ -1,9 +1,15 @@
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    unimplemented!("Encoding of {:?} in Atbash cipher.", plain);
+    unimplemented!(
+        "Encoding of {:?} in Atbash cipher.", 
+        plain
+    );
 }
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    unimplemented!("Decoding of {:?} in Atbash cipher.", cipher);
+    unimplemented!(
+        "Decoding of {:?} in Atbash cipher.", 
+        cipher
+    );
 }
