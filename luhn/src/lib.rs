b580e3dbf3a89e871f3609a081cdd30d7f1fa330
Zero Delta <zerodeltahedge@gmail.com>
2022-05-09 01:12:03 +0300

    1st passing iteration: luhn\src\lib.rs

--- luhn/src/lib.rs
+++ luhn/src/lib.rs
@@ -1,4 +1,17 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
-    unimplemented!("Is the Luhn checksum for {} valid?", code);
+    let sanitized = code.replace(" ", "");
+    let mut checksum = 0;
+    let mut count = 0;
+    for (i, c) in sanitized.char_indices() {
+        count += 1;
+        match ((sanitized.len() - i - 1) % 2, c.to_digit(10)) {
+            (0, Some(d))          => checksum += d,
+            (1, Some(d)) if d > 4 => checksum += 2 * d - 9,
+            (1, Some(d))          => checksum += 2 * d,
+            (_, _)                => return false
+        }
+    }
+    if count < 2 { false } 
+    else { checksum % 10 == 0 }
 }
