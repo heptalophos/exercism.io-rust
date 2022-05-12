927f1a1dee06cbef24865e699d82f42f10328e30
Zero Delta <zerodeltahedge@gmail.com>
2022-05-12 22:13:32 +0300

    run-length-encoding/lib.rs , 1st passing iteration

--- run-length-encoding/src/lib.rs
+++ run-length-encoding/src/lib.rs
@@ -1,7 +1,24 @@
+use itertools::Itertools;
+
 pub fn encode(source: &str) -> String {
-    unimplemented!("Return the run-length encoding of {}.", source);
+    let mut encoded = String::new();
+    for (ch, group) in source.chars().group_by(|c| *c).into_iter() 
+    {
+        match group.count() {
+            1 => encoded.push_str(format!("{}", ch).as_str()),
+            n => encoded.push_str(format!("{}{}", n, ch).as_str())
+        }                            
+    }
+    encoded
 }
 
 pub fn decode(source: &str) -> String {
-    unimplemented!("Return the run-length decoding of {}.", source);
+    let mut decoded = String::new();
+    for (n, c) in source.split(|c: char| !&c.is_numeric())
+                        .map(|n| n.parse().unwrap_or(1))
+                        .zip(source.matches(|c: char| !&c.is_numeric()))
+    {
+        decoded.push_str(format!("{}", c.repeat(n)).as_str())
+    }
+    decoded
 }
