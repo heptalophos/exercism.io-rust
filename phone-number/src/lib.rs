be1001c71be2496889dd0cf42d5352c98a41d4e5
Zero Delta <zerodeltahedge@gmail.com>
2022-07-06 18:02:04 +0300

    phone-number\src\lib.rs - passing

--- phone-number/src/lib.rs
+++ phone-number/src/lib.rs
@@ -1,3 +1,9 @@
 pub fn number(user_number: &str) -> Option<String> {
-    
+    let mut digits = user_number.chars().filter(|&c| c.is_digit(10))
+                                        .collect::<Vec<_>>();
+    if digits[0] == '1' { digits.remove(0); } 
+    if digits.len() != 10 || digits[0] < '2' || digits[3] < '2' { 
+        return None; 
+    }
+    Some (digits.into_iter().collect::<String>())
 }
