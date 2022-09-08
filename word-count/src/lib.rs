40858e612c2b551c1facbf48b463a536f819ebb4
Zero Delta <zerodeltahedge@gmail.com>
2022-09-08 17:58:18 +0300

    Update lib.rs

--- word-count/src/lib.rs
+++ word-count/src/lib.rs
@@ -2,5 +2,16 @@ use std::collections::HashMap;
 
 /// Count occurrences of words.
 pub fn word_count(words: &str) -> HashMap<String, u32> {
-    unimplemented!("Count of occurrences of words in {:?}", words);
+    let quote = '\'';
+    let boundary = |ch: char| ch.is_whitespace() ||
+                    ch.is_ascii_punctuation() && ch != quote; 
+    
+    words.to_lowercase().split(boundary)
+         .fold(HashMap::new(), |mut count, word| {
+            if !word.is_empty() {
+                *count.entry(word.trim_matches(quote).to_string())
+                      .or_insert(0) += 1;
+            }
+            count
+         })
 }
