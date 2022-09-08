cafaba6a5308d335ea651da9c4aa5f4d2f8b5a38
Zero Delta <zerodeltahedge@gmail.com>
2022-09-08 17:59:02 +0300

    Refactor word-count/lib.rs

--- word-count/src/lib.rs
+++ word-count/src/lib.rs
@@ -6,11 +6,13 @@ pub fn word_count(words: &str) -> HashMap<String, u32> {
     let boundary = |ch: char| ch.is_whitespace() ||
                     ch.is_ascii_punctuation() && ch != quote; 
     
-    words.to_lowercase().split(boundary)
+    words.split(boundary)
          .fold(HashMap::new(), |mut count, word| {
             if !word.is_empty() {
-                *count.entry(word.trim_matches(quote).to_string())
-                      .or_insert(0) += 1;
+                let sanitized = word.to_lowercase()
+                                    .trim_matches(quote)
+                                    .to_string();
+                *count.entry(sanitized).or_insert(0) += 1;
             }
             count
          })
