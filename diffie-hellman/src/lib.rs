f03d789cdbf1497fb990bd33cf904128bc816628
Zero Delta <zerodeltahedge@gmail.com>
2022-09-12 23:08:12 +0300

    Update lib.rs

--- diffie-hellman/src/lib.rs
+++ diffie-hellman/src/lib.rs
@@ -19,3 +19,16 @@ pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
         a
     )
 }
+fn modexp(base: u128, exp: u64, modulus: u64) -> u64 {
+    let mut b = base;
+    let mut e = exp;
+    let mut p = 1;
+    while e > 0 {
+        if e % 2 == 1 {
+            p = (b * p) % modulus as u128;
+        }
+        b = (b * b) % modulus as u128;
+        e << 1;
+    }
+    p as u64
+}
