5d7836238f6c37a545b360d91e8b7683744dc07f
Zero Delta <zerodeltahedge@gmail.com>
2022-09-12 23:13:31 +0300

    diffie-hellman\src\lib.rs, 1st passing iteration

--- diffie-hellman/src/lib.rs
+++ diffie-hellman/src/lib.rs
@@ -1,24 +1,17 @@
+use rand::Rng;
+
 pub fn private_key(p: u64) -> u64 {
-    unimplemented!("Pick a private key greater than 1 and less than {}", p)
+    rand::thread_rng().gen_range(2..p)
 }
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
-    unimplemented!(
-        "Calculate public key using prime numbers {} and {}, and private key {}",
-        p,
-        g,
-        a
-    )
+    modexp(g as u128, a, p)
 }
 
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
-    unimplemented!(
-        "Calculate secret key using prime number {}, public key {}, and private key {}",
-        p,
-        b_pub,
-        a
-    )
+    modexp(b_pub as u128, a, p)
 }
+
 fn modexp(base: u128, exp: u64, modulus: u64) -> u64 {
     let mut b = base;
     let mut e = exp;
@@ -28,7 +21,7 @@ fn modexp(base: u128, exp: u64, modulus: u64) -> u64 {
             p = (b * p) % modulus as u128;
         }
         b = (b * b) % modulus as u128;
-        e << 1;
+        e >>= 1;
     }
     p as u64
 }
