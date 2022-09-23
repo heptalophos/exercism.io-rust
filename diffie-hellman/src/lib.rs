fe82354cba3276117019180090e57697fb121ecf
Zero Delta <zerodeltahedge@gmail.com>
2022-09-23 04:19:41 +0300

    Refactor diffie-hellman\src\lib.rs

--- diffie-hellman/src/lib.rs
+++ diffie-hellman/src/lib.rs
@@ -1,5 +1,21 @@
 use rand::Rng;
 
+fn modexp(base: u128, exp: u64, modulus: u64) -> u64 {
+    let mut b = base; 
+    let mut e = exp; 
+    let mut p = 1;
+    while e > 0 {
+        if e & 1 == 1 { 
+            p *= b; 
+            p %= modulus as u128; 
+        }
+        e >>= 1; 
+        b *= b; 
+        b %= modulus as u128;
+    }
+    p as u64
+}
+
 pub fn private_key(p: u64) -> u64 {
     rand::thread_rng().gen_range(2..p)
 }
@@ -10,18 +26,4 @@ pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
 
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
     modexp(b_pub as u128, a, p)
-}
-
-fn modexp(base: u128, exp: u64, modulus: u64) -> u64 {
-    let mut b = base;
-    let mut e = exp;
-    let mut p = 1;
-    while e > 0 {
-        if e % 2 == 1 {
-            p = (b * p) % modulus as u128;
-        }
-        b = (b * b) % modulus as u128;
-        e >>= 1;
-    }
-    p as u64
 }
