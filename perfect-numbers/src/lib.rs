866cdc5e32db353cf5744ede187725a9ed58b69a
Zero Delta <zerodeltahedge@gmail.com>
2022-10-06 14:12:52 +0300

    Reformat perfect-numbers/src/lib.rs

--- perfect-numbers/src/lib.rs
+++ perfect-numbers/src/lib.rs
@@ -1,4 +1,5 @@
 #[derive(Debug, PartialEq, Eq)]
+
 pub enum Classification {
     Abundant,
     Perfect,
@@ -6,22 +7,14 @@ pub enum Classification {
 }
 
 pub fn classify(num: u64) -> Option<Classification> {
-    
     use std::cmp::Ordering;
-    
-    let aliquot: u64= 
-        (1..=(num / 2))
-        .filter(|i| num % i == 0)
-        .fold(0, |sum, i| sum + i);
-
+    let aliquot: u64= (1..=(num / 2)).filter(|i| num % i == 0)
+                                     .fold(0, |sum, i| sum + i);
     if num != 0 {
         return match aliquot.cmp(&num) {
-            Ordering::Greater => 
-                Some(Classification::Abundant),
-            Ordering::Equal => 
-                Some(Classification::Perfect),
-            Ordering::Less => 
-                Some(Classification::Deficient)
+            Ordering::Greater => Some(Classification::Abundant),
+            Ordering::Equal   => Some(Classification::Perfect),
+            Ordering::Less    => Some(Classification::Deficient)
         }
     } 
     None
