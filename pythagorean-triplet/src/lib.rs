9239d241f8f625c050ab0a86f059b9c830a539a9
Zero Delta <zerodeltahedge@gmail.com>
2022-10-06 13:30:21 +0300

    Refactor pythagorean-triplet\src\lib.rs

--- pythagorean-triplet/src/lib.rs
+++ pythagorean-triplet/src/lib.rs
@@ -1,19 +1,17 @@
 use std::collections::HashSet;
 
 pub fn find(sum: u32) -> HashSet<[u32; 3]> {
-    
     let mut triplets = HashSet::<[u32; 3]>::new();
-    
-    for a in 0..=sum / 3 {
-        for b in a + 1..=(sum - a) / 2 {
-            let c = sum - (a + b);
-            let strict = a < b && b < c;
-            let pythagorean = c * c == a * a + b * b;
-            if strict && pythagorean {
-                triplets.insert([a, b, c]);
-            }
+    let mut a = 3;
+    while a <= sum / 3 {
+        let b = ((sum * sum) - (2 * sum * a)) / (2 * (sum - a));
+        let r = ((sum * sum) - (2 * sum * a)) % (2 * (sum - a));
+        let c = sum - (a + b);
+        if r == 0 && a < b {
+            triplets.insert([a, b, c]);
+            a += 2;
         }
+        else { a += 1; }
     }
-    
     triplets
 }
