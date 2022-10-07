b66e0cb54095e808d6a2b52fe74afc7dfb9790cc
Zero Delta <zerodeltahedge@gmail.com>
2022-10-07 20:24:13 +0300

    minesweeper\src\lib.rs, 1st passing iteration

--- minesweeper/src/lib.rs
+++ minesweeper/src/lib.rs
@@ -1,3 +1,32 @@
+const MINE: char = '*';
+
 pub fn annotate(minefield: &[&str]) -> Vec<String> {
-    unimplemented!("\nAnnotate each square of the given minefield with the number of mines that surround said square (blank if there are no surrounding mines):\n{:#?}\n", minefield);
+    minefield.iter().enumerate()
+             .map(|(r, row)| {
+                    row.chars().enumerate()
+                    .map(|(c, tile)| 
+                            match tile {
+                                '*' => MINE,
+                                 _ => tile_value(r, c, minefield)
+                    }).collect()
+             }).collect()
+}
+
+fn tile_value(row: usize, col: usize, field: &[&str]) -> char {
+    let mut mine_count = 0; 
+    for r in row.saturating_sub(1)..=row + 1 {
+        for c in col.saturating_sub(1)..=col + 1 {
+            if tile(r, c, field) == Some( MINE ) {
+                mine_count += 1;
+            }
+        }
+    }
+    match mine_count {
+        0 => ' ',
+        _ =>  char::from(48u8 + mine_count)
+    }
+} 
+
+fn tile(r: usize, c: usize, field: &[&str]) -> Option<char> {
+    field.get(r).and_then(|row| row.chars().nth(c))
 }
