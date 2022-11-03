a70e0134cc5cdd84f2aad403dff1af079500c095
Zero Delta <zerodeltahedge@gmail.com>
2022-11-03 21:28:54 +0200

    Reformat crypto-square\src\lib.rs

--- crypto-square/src/lib.rs
+++ crypto-square/src/lib.rs
@@ -3,14 +3,16 @@ pub fn encrypt(input: &str) -> String {
         input.chars().filter(char::is_ascii_alphanumeric)
              .map(|ch| ch.to_ascii_lowercase())
              .collect::<Vec<_>>();
-        if sanitized_input_vector.is_empty() {String::new()}
-        else {
-            (0..square_side(sanitized_input_vector.len()))
-            .map(|c| sanitized_input_vector
-                     .chunks(square_side(sanitized_input_vector.len()))
-                     .map(|r| r.get(c).unwrap_or(&' ')).collect::<String>())
-                     .collect::<Vec<_>>().join(" ")
-        }
+    
+    if sanitized_input_vector.is_empty() { String::new() } 
+    else { (0..square_side(sanitized_input_vector.len()))
+           .map(|c| 
+                 sanitized_input_vector
+                 .chunks(square_side(sanitized_input_vector.len()))
+                 .map(|r| r.get(c).unwrap_or(&' '))
+                 .collect::<String>())
+                 .collect::<Vec<_>>().join(" ") 
+    }
 }
 
 fn square_side(len: usize) -> usize {
