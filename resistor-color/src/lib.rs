66166fe131cf0fb9db14b933bd2cf6f40c3be3ba
Zero Delta <zerodeltahedge@gmail.com>
2022-11-28 01:38:19 +0200

    Refactor resistor-color

--- resistor-color/src/lib.rs
+++ resistor-color/src/lib.rs
@@ -1,30 +1,31 @@
-use enum_iterator::IntoEnumIterator;
-// use int_enum::IntEnum;
+use enum_iterator::{all, Sequence};
+use int_enum::IntEnum;
 
-#[derive(Debug, PartialEq, IntoEnumIterator)]
+#[repr(u32)]
+#[derive(Debug, PartialEq, Copy, Clone, IntEnum, Sequence)]
 pub enum ResistorColor {
     Black = 0,
-    Brown,
-    Red,
-    Orange,
-    Yellow,
-    Green,
-    Blue,
-    Violet,
-    Grey,
-    White,
+    Brown = 1,
+    Red = 2, 
+    Orange = 3,
+    Yellow = 4,
+    Green = 5,
+    Blue = 6,
+    Violet = 7,
+    Grey = 8,
+    White = 9
 }
 
-pub fn color_to_value(_color: ResistorColor) -> usize {
-    _color as usize
+pub fn color_to_value(_color: ResistorColor) -> u32 {
+    _color as u32
 }
 
-pub fn value_to_color_string(value: usize) -> String {
+pub fn value_to_color_string(value: u32) -> String {
     if value >= 10 { return String::from("value out of range"); }
-    let some_color = ResistorColor::into_enum_iter().nth(value);
+    let some_color = ResistorColor::from_int(value.try_into().unwrap());
     format!("{:?}", some_color.unwrap())
 }
 
 pub fn colors() -> Vec<ResistorColor> {
-    ResistorColor::into_enum_iter().collect::<Vec<ResistorColor>>()
+    all::<ResistorColor>().collect::<Vec<ResistorColor>>()
 }
