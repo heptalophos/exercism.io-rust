58e291e054ce9cd540528651023de0e997ab87ca
Zero Delta <zerodeltahedge@gmail.com>
2023-08-11 13:26:01 +0300

    Update secret-handshake.rs

--- secret-handshake/tests/secret-handshake.rs
+++ secret-handshake/tests/secret-handshake.rs
@@ -6,49 +6,49 @@ fn wink_for_1() {
 }
 
 #[test]
-// #[ignore]
+#[ignore]
 fn double_blink_for_10() {
     assert_eq!(actions(2), vec!["double blink"])
 }
 
 #[test]
-// #[ignore]
+#[ignore]
 fn close_your_eyes_for_100() {
     assert_eq!(actions(4), vec!["close your eyes"])
 }
 
 #[test]
-// #[ignore]
+#[ignore]
 fn jump_for_1000() {
     assert_eq!(actions(8), vec!["jump"])
 }
 
 #[test]
-// #[ignore]
+#[ignore]
 fn combine_two_actions() {
     assert_eq!(actions(3), vec!["wink", "double blink"])
 }
 
 #[test]
-// #[ignore]
+#[ignore]
 fn reverse_two_actions() {
     assert_eq!(actions(19), vec!["double blink", "wink"])
 }
 
 #[test]
-// #[ignore]
+#[ignore]
 fn reversing_one_action_gives_the_same_action() {
     assert_eq!(actions(24), vec!["jump"])
 }
 
 #[test]
-// #[ignore]
+#[ignore]
 fn reversing_no_actions_still_gives_no_actions() {
     assert_eq!(actions(16), Vec::<&'static str>::new())
 }
 
 #[test]
-// #[ignore]
+#[ignore]
 fn all_possible_actions() {
     assert_eq!(
         actions(15),
@@ -57,7 +57,7 @@ fn all_possible_actions() {
 }
 
 #[test]
-// #[ignore]
+#[ignore]
 fn reverse_all_possible_actions() {
     assert_eq!(
         actions(31),
@@ -66,7 +66,7 @@ fn reverse_all_possible_actions() {
 }
 
 #[test]
-// #[ignore]
+#[ignore]
 fn do_nothing_for_zero() {
     assert_eq!(actions(0), Vec::<&'static str>::new())
 }
