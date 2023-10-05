6314baa71a79226d3e55961efc722e5b6f73f28c
Zero Delta <zerodeltahedge@gmail.com>
2023-10-06 00:54:17 +0300

    Refactor armstrong-numbers

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,11 +1,8 @@
 pub fn is_armstrong_number(num: u64) -> bool {
-       
-       num.to_string().chars()
-          .fold(0, |mut acc, d| 
-           {
-              acc += (d.to_digit(10).unwrap() as u64)
-                     .pow((((num as f32).log10() as u32) + 1) as u32); 
-              acc
-           }) 
-       == num
+       num == num.to_string().chars()
+                 .fold(0, |mut acc, d| {
+                     acc += (d.to_digit(10).unwrap() as u64)
+                            .pow((((num as f32).log10() as u32) + 1) as u32); 
+                     acc
+                  })
 }
