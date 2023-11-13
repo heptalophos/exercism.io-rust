4637295a0eb2335a493dcf313d8cbb2a31a45141
Zero Delta <zerodeltahedge@gmail.com>
2023-11-13 16:56:20 +0200

    Refactor lib.rs

--- kindergarten-garden/src/lib.rs
+++ kindergarten-garden/src/lib.rs
@@ -1,10 +1,12 @@
 pub fn plants(diagram: &str, student: &str) -> Vec<&'static str> {
-    let child_plants = (2 * (student.bytes().next().unwrap() - b'A')).into();
+    let child_cups = 
+        (2 * (student.bytes().next().unwrap_or(0) - b'A')).into();
+    
     diagram
     .lines()
     .flat_map(
         |line| {
-            line[child_plants..=child_plants + 1]
+            line[child_cups..=child_cups + 1]
             .chars()
             .map(|p| match p {
                     'G' => "grass", 
