dfe2740a1c58015babbca474b8a27b52cf8f86d1
Zero Delta <zerodeltahedge@gmail.com>
2023-11-22 10:28:57 +0200

    Update lib.rs

--- collatz-conjecture/src/lib.rs
+++ collatz-conjecture/src/lib.rs
@@ -1,12 +1,11 @@
 pub fn collatz(n: u64) -> Option<u64> {
     if n <= 0 { return None }
     let mut steps = 0u64;
-    let mut n = n;
+    let mut n:u64 = n;
     while n > 1 {
         match n % 2 == 0 {
             true  => n >>= 1,
-            false => n = n.checked_mul(3)?
-                          .checked_add(1)?
+            false => n = n.checked_mul(3)?.checked_add(1)?
         }
         steps += 1
     } 
