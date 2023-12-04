1b5382762e9d8fa624812d29bc3d1a158158b48a
Zero Delta <zerodeltahedge@gmail.com>
2023-12-04 05:04:39 +0200

    Update lib.rs

--- knapsack/src/lib.rs
+++ knapsack/src/lib.rs
@@ -3,6 +3,25 @@ pub struct Item {
     pub value: u32,
 }
 
-pub fn maximum_value(_max_weight: u32, _items: &[Item]) -> u32 {
-    todo!("Solve the knapsack exercise");
+pub fn maximum_value(max_weight: u32, items: &[Item]) -> u32 {
+    max_value(max_weight, 0, &items.iter().collect::<Vec<_>>())
+}
+
+fn max_value(max_weight: u32, tot_value: u32, items: &[&Item]) -> u32 {
+    items.iter()
+         .enumerate()
+         .filter_map(|(i, item)| {
+            if item.weight > max_weight {
+                Some (tot_value)
+            }
+            else { 
+            let mut remaining = items.to_vec();
+            remaining.remove(i);
+            Some (max_value(max_weight - item.weight, 
+                            tot_value + item.value, 
+                            &remaining))
+            }
+          })
+         .max()
+         .unwrap_or_default()
 }
